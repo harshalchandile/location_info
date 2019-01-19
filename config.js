568193bacc38cb09ca0e52636918d0fd285982b9
harshal chandile <harshal.chandile767@gmail.com>
2019-01-19 20:41:54 +0530

    Env port config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-    port: 3000,
+    port: process.env.PORT || 3000,
     api_key: 'AIzaSyBQcPv2pGeRhcTu7c5qQqMx96C9hPhMyzw'
 }
 module.exports = config
--- geo_location/config.js
+++
@@ -1,5 +0,0 @@
-let config = {
-    port: 3000,
-    api_key: 'AIzaSyBQcPv2pGeRhcTu7c5qQqMx96C9hPhMyzw'
-}
-module.exports = config
